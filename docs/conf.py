5ca9468c52fa1667b07cd5834f3f1946c74e74ba
Egor_Kozlyakovsky <egorgolik0308@mail.ru>
2025-04-30 19:55:40 +0300

    add project documentation

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Chat_Project'
+copyright = '2025, Egor and Andrey'
+author = 'Egor and Andrey'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # авто документации из docstrings
+    'sphinx.ext.viewcode',  # ссылки на исходный код
+    'sphinx.ext.napoleon',  # поддержка Google и NumPy стиля документации
+    'sphinx.ext.todo',  # поддержка TODO
+    'sphinx.ext.coverage',  # проверяет покрытие документации
+    'sphinx.ext.ifconfig',  # условные директивы в документации
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+todo_include_todos = True  # показывать TODO в готовой документации
